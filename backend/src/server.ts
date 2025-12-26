2fba5baa51774844c70bdbed9e9ca33344b49e35
Artem <ar.gubarev50@gmail.com>
2025-12-27 01:11:42 +0500

    Initial commit: trainer mini app2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
-// src/server.ts
+// backend/src/server.ts
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server started on port ${PORT}`);
 });
